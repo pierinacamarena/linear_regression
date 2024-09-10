8eb1fb4daff24ce6ad5a4c5902922589661804a2
pierinacamarena <pcamaren@student.42.fr>
2024-09-10 20:53:35 +0200

    [ADD]: Read of dataset file

--- model_training/main.py
+++ model_training/main.py
@@ -1,16 +1,30 @@
 import argparse
 
-def read_dataset() -> any:
-
+def read_dataset() -> str:
+    """
+    Read dataset
+    """
+    try:
+        # Using with automatically closes the file after the block is executed, even if an error occurs.
+        with open("data/data.csv", "r") as dataset:
+            dataset = dataset.read()
+            print(f'dataset is {dataset}')
+            return dataset
+    except Exception as e:
+        print(f'Error: Opening file data.csv - {e}')
+        return ''
 
 
 def main():
+
     # Parse input arguments 
     parser = argparse.ArgumentParser(description="Receive mileag of a car")
     parser.add_argument('mileage', type=int, help="The mileage for a car")
     args = parser.parse_args()
 
     mileage = args.mileage
+    print(mileage)
+    read_dataset()
 
 if __name__ == "__main__":
     main()
